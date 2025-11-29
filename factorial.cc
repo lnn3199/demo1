a6197492e83dfdd01575993f98b580c509893374
Your Name <you@example.com>
2025-11-29 12:32:06 +0800

    demo2

---
+++ factorial.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+long long factorial(int n){
+    if(n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+long long factorialSum(int n){
+    if(n == 1)
+        return 1;
+    return factorial(n)+factorialSum(n-1);
+}
+
+int main()
+{
+    int n = 5;
+    cout<<factorialSum(5);
+    return 0;
+}
